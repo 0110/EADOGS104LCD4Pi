6010dc9307ed7cf05519340aa495d2893d842705
Ollo <ollo@ollo.mobil>
2020-06-17 21:10:45 +0200

    Added commandline -s to read data from stdin

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include "ssd1803a-spi.h"
+
+#define MAX_CHARACTERS	(20*4)
 
 int main(int argc, char ** argv) {
  
@@ -15,13 +19,30 @@ int main(int argc, char ** argv) {
 
  ssd1803a_spi_init();
  
- ret = ssd1803a_spi_sendText("Hello World", sizeof("Hello World"));
- if (ret == SSD1803A_RET_OK) {
-     printf("Display should show 'Hello World'\n");
+ if (argc >= 2) {
+	if (strcmp(argv[1], "-s") == 0) {
+		char buffer[MAX_CHARACTERS];
+		int readbytes=read(STDIN_FILENO, buffer, MAX_CHARACTERS);
+		ret = ssd1803a_spi_sendText(buffer, readbytes);
+		if (readbytes < MAX_CHARACTERS) {
+			buffer[readbytes] = '\0';
+		}
+		if (ret == SSD1803A_RET_OK) {
+		     printf("Display should show '%s' (%d characters)\n", buffer, readbytes);
+		} else {
+		     printf("Display returned code %d\n", ret);
+		}
+	} else {
+		printf("Unkown command %s\n", argv[1]);
+	}
  } else {
-     printf("Display returned code %d\n", ret);
+	 ret = ssd1803a_spi_sendText("Hello World\0", sizeof("Hello World") - 1);
+	 if (ret == SSD1803A_RET_OK) {
+	     printf("Display should show 'Hello World'\n");
+	 } else {
+	     printf("Display returned code %d\n", ret);
+	 }
  }
-
  ssd1803a_spi_close();
 
  return 0;
